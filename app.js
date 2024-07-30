0e407665c26f1ef5b79dd6dae3d97230d324f034
minsuwai <mswwai15@gmail.com>
2024-07-30 18:02:08 +0630

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxivFgCgx-k43cp5NTXOKNFkc3LSTNObbzp4AmEp4L9v2Pcu8nXOI2js7vMzUmiQ7jbag/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Thank You For Subscribing!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
